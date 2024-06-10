797bcc3f28838127b347fbc7b2e9d399195b0f22
Edber Vuciterna <vuciternaedber@gmail.com>
2024-06-11 01:35:08 +0200

    vercel configuration for deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // This is important for Vercel deployment
+  build: {
+    outDir: 'dist', // Default output directory
+  },
   plugins: [react()],
   server: {
     port: 3000, // Change this to your desired port
